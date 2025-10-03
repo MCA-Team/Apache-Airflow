b2bd956e20dfc946a3e9b7abd46825bcf3692179
adotri-frimpong <goldmalcom43@gmail.com>
2025-10-03 12:54:34 +0200

    [FEAT] Creation of a DAG (first_dag.py)

---
+++ dags/first_dag.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators import BashOperator
+from datetime import datetime
+
+
+with DAG(
+    dag_id = "DAG v1",
+    description = "Test dag",
+    start_date = datetime(2025, 10, 3, 14),
+    schedule_interval = "@daily"
+) as dag:
+    task1 = BashOperator(
+        task_id = "task1",
+        bash_command = "echo 'hello it's task 1'"
+    )
+
+    task2 = BashOperator(
+        task_id = "task2",
+        bash_command = "echo 'hello it's task 2'"
+    )
+
+    task3 = BashOperator(
+        task_id = "task3",
+        bash_command = "echo 'hello it's task 3'"
+    )
+
+    task1 >> [task2, task3]
